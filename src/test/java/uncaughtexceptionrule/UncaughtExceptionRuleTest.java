b62a40536acf7a98c8c2a5ffe510456ace985d20
reza-bayat <bayat@inside.sahab.ir>
2019-01-09 18:03:45 +0330

    implement UncaughtExceptionRule

---
+++ src/test/java/uncaughtexceptionrule/UncaughtExceptionRuleTest.java
@@ -0,0 +1,35 @@
+package uncaughtexceptionrule;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class UncaughtExceptionRuleTest {
+
+    @Rule
+    public UncaughtExceptionRule rule = new UncaughtExceptionRule();
+
+    @Test
+    public void testAssertOnUnhandledException() throws InterruptedException {
+        Thread t = new Thread(() -> {
+            int ignored = 10 / 0;
+        });
+        t.start();
+        t.join();
+
+        Assert.assertNotNull(rule.getException());
+        Assert.assertTrue(rule.getException() instanceof ArithmeticException);
+        rule.clearException();
+    }
+
+    // It is a test to test failure of test. So it is commented.
+    // If you are in doubt, you can uncomment it!
+    // @Test
+    public void testFailureForUnhandledException() throws InterruptedException {
+        Thread t = new Thread(() -> {
+            int ignored = 10 / 0;
+        });
+        t.start();
+        t.join();
+    }
+}
