cb0ee93daed1b7c9cd95ffebd897622d5b9648c6
Mojtaba Zarezadeh <m.zarezadeh@inside.sahab.ir>
2022-08-28 12:56:25 +0430

    Fix: fix serialVersionUID warning.

--- src/main/java/ir/sahab/uncaughtexceptionrule/AggregateException.java
+++ src/main/java/ir/sahab/uncaughtexceptionrule/AggregateException.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 class AggregateException extends Exception {
+    static final long serialVersionUID = -7188848759039620762L;
     private final List<Throwable> exceptions;
 
     public AggregateException(String message, List<Throwable> exceptions) {
@@ -13,8 +14,7 @@ class AggregateException extends Exception {
 
     @Override
     public String getMessage() {
-        final String messages = exceptions.stream().map(Throwable::getMessage)
-                .collect(Collectors.joining(",\n\t", "List of Exception messages:\n\t", ""));
+        final String messages = exceptions.stream().map(Throwable::getMessage).collect(Collectors.joining(",\n\t", "List of Exception messages:\n\t", ""));
         return String.format("%s\n\t%s", super.getMessage(), messages);
     }
 
